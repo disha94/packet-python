0d31e2ed98c0806c7d199a4c7f63a9b1933e86f0
Disha <dsinghal@criterionnetworks.com>
2016-11-04 21:54:09 +0530

    debug

--- packet/Manager.py
+++ packet/Manager.py
@@ -141,14 +141,14 @@ class Manager(BaseAPI):
         data = self.call_api("capacity", params=params)
         capacities = list()
         data_cap = json.dumps(data)
-        for jsoned in data_cap:
-            print "j"+ jsoned
-            print data
-            print data_cap
-            print data_cap['capacity']
-            capacity = Capacity(jsoned)
-            capacities.append(capacity)
-            print capacities
+        #for jsoned in data_cap:
+         #   print "j"+ jsoned
+        print data
+        print data_cap
+        #print data_cap['capacity']
+        capacity = Capacity(data_cap)
+        capacities.append(capacity)
+        print capacities
         return capacities
 
     def check_capacity(self, facility, plan, quantity):
